ec36abacf13783afd5e2e7555b7a0543d3016632
Drew Moseley <drew@moseleynet.net>
2025-04-10 17:48:45 -0400

    Setup for multiarch builds.
    Signed-off-by: Drew Moseley <drew@moseleynet.net>

--- build-it.sh
+++ build-it.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 #
 
-docker buildx build --push --platform linux/amd64 -t drewmoseley/yocto-docker:20.04 .
-docker buildx build --push --platform linux/arm64 -t drewmoseley/yocto-docker:20.04 .
+docker buildx build --push --platform linux/amd64,linux/arm64 -t drewmoseley/yocto-docker:20.04 .
