ceabfcbc95ac907eb72221cea3546d85cfbe9887
Drew Moseley <drew@moseleynet.net>
2025-04-10 14:11:20 -0400

    Switch to Ubuntu 22.04
    Signed-off-by: Drew Moseley <drew@moseleynet.net>

--- build-it.sh
+++ build-it.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
 
-docker buildx build --push --platform linux/amd64 -t drewmoseley/yocto-docker:20.04 .
-docker buildx build --push --platform linux/arm64 -t drewmoseley/yocto-docker:20.04 .
+docker buildx build --push --platform linux/amd64 -t drewmoseley/yocto-docker:22.04 .
+docker buildx build --push --platform linux/arm64 -t drewmoseley/yocto-docker:22.04 .
