69bc62239e56d0cffa1f60c8b3c21167db60c1c7
Drew Moseley <drew@moseleynet.net>
2025-04-10 14:12:05 -0400

    Specific Ubuntu version as my container tag
    Signed-off-by: Drew Moseley <drew@moseleynet.net>

--- build-it.sh
+++ build-it.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 #
 
-docker buildx build --push --platform linux/amd64,linux/arm64 -t drewmoseley/yocto-docker:latest .
+docker buildx build --push --platform linux/amd64 -t drewmoseley/yocto-docker:20.04 .
+docker buildx build --push --platform linux/arm64 -t drewmoseley/yocto-docker:20.04 .
